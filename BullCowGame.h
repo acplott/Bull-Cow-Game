0b469d4c33a66b2d63eabd436ca9ded89181962b
acplott <acplott@gmail.com>
2017-03-30 01:20:55 -0400

    Update BullCowGame.h

--- BullCowGame.h
+++ BullCowGame.h
@@ -1,1 +1,51 @@
+/* The game logic (no view code or direct user interaction)
+The game is a simple guess the word game based on Mastermind.
+*/
 
+#pragma once
+#include <string>
+
+using FString = std::string; //conforms to unreal standards
+using int32 = int;
+
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+	Invalid_Status,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase,
+};
+
+class FBullCowGame
+{
+public:
+	FBullCowGame();
+
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+	bool IsGameWon() const;
+
+	void Reset(); 
+	EGuessStatus CheckGuessValidity(FString) const; 
+
+	//counts bulls and cows and increases try #, assuming valid guess
+	FBullCowCount SubmitValidGuess(FString);
+
+
+private:
+	int32 MyMaxTries;
+	int32 MyCurrentTry;
+	FString MyHiddenWord;
+	bool bGameIsWon;
+	bool IsLowerCase(FString) const;
+	bool IsIsogram(FString) const;
+
+};
